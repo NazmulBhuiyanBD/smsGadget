368fc48cf749344b166f47e433149e608388b116
Md Nazmul Haque Bhuiyan <127285688+NazmulBhuiyanBD@users.noreply.github.com>
2024-12-21 13:22:38 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -91,24 +91,21 @@
     ?>
     <!-- Cart element  -->
     <div class="modal fade" tabindex="-1" id="cart">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <h5 class="text-center mt-3">WELCOME TO CART</h5>
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <h5 class="text-center mt-3">Your Cart</h5>
             <div class="modal-body">
-                <form method="POST" action="php/login.php">
-                    
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                  </form>
-                
+                <!-- Container to display cart items -->
+                <div id="cart-items"></div>
+                <button type="button" class="btn btn-primary w-100 mt-3">Checkout</button>
             </div>
             <div class="modal-footer">
-              
-              <p>Happy shopping. Best of luck</p> 
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
             </div>
-          </div>
         </div>
-      </div>
+    </div>
+</div>
+
 
     <!-- header element -->
        <div class="container-fluid navbar shadow-sm">
@@ -424,6 +421,7 @@
                     </div>
                 </div>
           </div>
+    <script src=js/cart.js></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
